a8e2521fe0b380e3b74ac6c2093d5098306bf1b1
Samantha Diaz <samidiaz3@gmail.com>
2022-02-04 13:36:14 -0500

    fable to get pegs from board array serializer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // o	npm install -g json-server
 // o	json-server --watch db.json
 
-const tilesIndexEndPoint = "http://localhost:3000/tiles"
-
+// const tilesIndexEndPoint = "http://localhost:3000/tiles"
+const boardShowEndPoint = "http://localhost:3000/boards/1"
 
 // make sure the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,28 +12,39 @@ document.addEventListener('DOMContentLoaded', () => {
 })
     // fetch your endpoint
 
-function getTiles() {
-    fetch(tilesIndexEndPoint)
+// function getTiles() {
+//     fetch(tilesIndexEndPoint)
+//     .then(response => response.json())
+//     .then(tilesArray => {
+//         console.log(tilesArray)
+//         tilesArray.data.forEach( tile => {
+//             const tileMarkup = `
+//             <div data-id=${tile.id}>
+//                 <p>ID: ${tile.id}</p>
+//                 <p>peg: ${tile.attributes.peg}</p>
+//             </div>
+//             `
+//         document.querySelector('#tile-container').innerHTML += tileMarkup
+//         })
+
+//     })
+// } 
+
+function getBoard() {
+    fetch(boardShowEndPoint)
     .then(response => response.json())
-    .then(tilesArray => {
-        console.log(tilesArray)
-        tilesArray.data.forEach( tile => {
+    .then(boardArray => {
+        console.log(boardArray)
+        boardArray.data.attributes.tiles.forEach( tile => {
             const tileMarkup = `
             <div data-id=${tile.id}>
                 <p>ID: ${tile.id}</p>
-                <p>peg: ${tile.attributes.peg}</p>
+                <p>peg: ${tile.peg}</p>
+                <p>number: ${tile.number}</p>
             </div>
             `
-        document.querySelector('#tile-container').innerHTML += tileMarkup
+            document.querySelector('#pegs').innerHTML += tileMarkup
         })
 
     })
 } 
-
-
-function getBoard() {
-    console.log("board!")
-}
-   
-
-
