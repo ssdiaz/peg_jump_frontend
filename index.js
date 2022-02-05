f3a0dc2fe451d5df8e67c24b258b8640e0816135
Samantha Diaz <samidiaz3@gmail.com>
2022-02-04 21:43:16 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -15,25 +15,25 @@
 // let peg14 = document.querySelector("#pegs > table > tbody > tr:nth-child(5) > td:nth-child(7)")
 // let peg15 = document.querySelector("#pegs > table > tbody > tr:nth-child(5) > td:nth-child(9)")
 
-let peg1  = "#pegs > table > tbody > tr:nth-child(1) > td:nth-child(5)"
-let peg2  = "#pegs > table > tbody > tr:nth-child(2) > td:nth-child(4)"
-let peg3  = "#pegs > table > tbody > tr:nth-child(2) > td:nth-child(6)"
-let peg4  = "#pegs > table > tbody > tr:nth-child(3) > td:nth-child(3)"
-let peg5  = "#pegs > table > tbody > tr:nth-child(3) > td:nth-child(5)"
-let peg6  = "#pegs > table > tbody > tr:nth-child(3) > td:nth-child(7)"
-let peg7  = "#pegs > table > tbody > tr:nth-child(4) > td:nth-child(2)"
-let peg8  = "#pegs > table > tbody > tr:nth-child(4) > td:nth-child(4)"
-let peg9  = "#pegs > table > tbody > tr:nth-child(4) > td:nth-child(6)"
-let peg10 = "#pegs > table > tbody > tr:nth-child(4) > td:nth-child(8)"
-let peg11 = "#pegs > table > tbody > tr:nth-child(5) > td:nth-child(1)"
-let peg12 = "#pegs > table > tbody > tr:nth-child(5) > td:nth-child(3)"
-let peg13 = "#pegs > table > tbody > tr:nth-child(5) > td:nth-child(5)"
-let peg14 = "#pegs > table > tbody > tr:nth-child(5) > td:nth-child(7)"
-let peg15 = "#pegs > table > tbody > tr:nth-child(5) > td:nth-child(9)"
+let peg1  = "#board > table > tbody > tr:nth-child(1) > td:nth-child(5)"
+let peg2  = "#board > table > tbody > tr:nth-child(2) > td:nth-child(4)"
+let peg3  = "#board > table > tbody > tr:nth-child(2) > td:nth-child(6)"
+let peg4  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(3)"
+let peg5  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(5)"
+let peg6  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(7)"
+let peg7  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(2)"
+let peg8  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(4)"
+let peg9  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(6)"
+let peg10 = "#board > table > tbody > tr:nth-child(4) > td:nth-child(8)"
+let peg11 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(1)"
+let peg12 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(3)"
+let peg13 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(5)"
+let peg14 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(7)"
+let peg15 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(9)"
 
 let pegArray = [peg1, peg2, peg3, peg4, peg5, peg6, peg7, peg8, peg9, peg10, peg11, peg12, peg13, peg14, peg15]
 
-const boardShowEndPoint = "http://localhost:3000/boards/1"
+const boardShowEndPoint = "http://localhost:3000/boards/1";
 // make sure the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM loaded")
@@ -48,9 +48,9 @@ function getBoard() {
   .then(boardArray => {
     console.log(boardArray)
     boardArray.data.attributes.tiles.forEach( tile => {
-      const tileMarkup = `
       
-      <div id="pegs">
+      const tileMarkup = `      
+      <div id="board">
       <button class="peg" id=${tile.id}>
           ID: ${tile.id}
           <br>peg: ${tile.peg}
@@ -71,23 +71,26 @@ function getBoard() {
 // const allPegs = document.querySelectorAll(".peg");
 
 // works!!
-const pegs = document.getElementById('pegs');
-pegs.addEventListener('click', (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
-  if (!isButton) {
-    return;
+const board = document.getElementById('board');
+board.addEventListener('click', (event) => {
+  // event.target === button#1.peg
+  // event.target.nodeName === BUTTON
+
+  // check that clicked button and not on div
+  const objClicked = event.target.nodeName 
+  if (objClicked === 'BUTTON') {
+    console.dir(event.target.id);
+
+    // call a function here
   }
 
-  console.dir(event.target.id);
-  console.log("hi")
-  // call a function here
 })
 
 
 
 
 
-
+// for play button - right now changes background
 const btn = document.querySelector('button');
 function random(number) {
   return Math.floor(Math.random() * (number+1));
