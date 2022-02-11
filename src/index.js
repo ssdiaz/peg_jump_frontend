c010dd50eb1378ca7568fe620f5d2ff170ba8825
Samantha Diaz <samidiaz3@gmail.com>
2022-02-11 13:12:55 -0500

    fixed how to get pegs from index.html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-let peg1  = "#board > table > tbody > tr:nth-child(1) > td:nth-child(5)"
-let peg2  = "#board > table > tbody > tr:nth-child(2) > td:nth-child(4)"
-let peg3  = "#board > table > tbody > tr:nth-child(2) > td:nth-child(6)"
-let peg4  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(3)"
-let peg5  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(5)"
-let peg6  = "#board > table > tbody > tr:nth-child(3) > td:nth-child(7)"
-let peg7  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(2)"
-let peg8  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(4)"
-let peg9  = "#board > table > tbody > tr:nth-child(4) > td:nth-child(6)"
-let peg10 = "#board > table > tbody > tr:nth-child(4) > td:nth-child(8)"
-let peg11 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(1)"
-let peg12 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(3)"
-let peg13 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(5)"
-let peg14 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(7)"
-let peg15 = "#board > table > tbody > tr:nth-child(5) > td:nth-child(9)"
-let pegArray = [peg1, peg2, peg3, peg4, peg5, peg6, peg7, peg8, peg9, peg10, peg11, peg12, peg13, peg14, peg15]
-
 // Constants used throughout
 const board = document.getElementById('board'); 
 const btnPlay = document.querySelector("#play-btn");
@@ -201,6 +184,18 @@ function cheatWin(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 //  [TILES] loads tiles
 function getGameTiles() {
   fetch("http://localhost:3000/games")
@@ -228,13 +223,22 @@ function getGameTiles() {
 
       data.attributes.tiles.forEach( tileData =>  {
         let newTile = new Tile(tileData)
-        document.querySelector(pegArray[`${tileData.id}`-1]).innerHTML += newTile.renderPegHTML()
+        document.querySelector(`#tile${newTile.number}`).innerHTML += newTile.renderPegHTML()
         newTile.renderPegElements() 
       })
 
     })
   })
 }
+
+
+
+
+
+
+
+
+
 
 
 
